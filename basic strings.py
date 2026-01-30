dcc4af7844a32c072640ec3fe3b9ecd6642cebbf
alkygerolymatou <alky.gerolymatou@gmail.com>
2026-01-30 11:48:45 +0100

    Initial commit

---
+++ basic strings.py
@@ -0,0 +1,16 @@
+#from cgi importform
+text = "hello world"
+print(text)
+text= "hello world 2"
+print(text)
+print(text[4])
+print(len(text)) # what is the lenght of the text
+text= ""
+print(len(text))
+text="Bob"
+print(text[-1]) #last charaxter in the strint
+print(text[-3])
+
+#print(text[3]) #this is an error
+print(text[6//3])
+
